e7b0594b5647ad1021d2b808a01175ab616810cf
shubhanshu-02 <shubhanshu004@gmail.com>
2025-02-23 19:40:58 +0530

    ringtone player working as intended

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,106 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:permission_handler/permission_handler.dart';
+import 'package:flutter_ringtone_player/flutter_ringtone_player.dart';
 
-void main() {
-  runApp( MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-   MyApp({super.key});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Notification Test'),
-        ),
+        // appBar: AppBar(
+        //   title: const Text('Ringtone player'),
+        // ),
         body: Center(
-          child: ElevatedButton(
-            child: const Text('Request Notification Permission'),
-            onPressed: () {
-              requestNotificationPermission();
-            },
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: ElevatedButton(
+                    child: const Text('playAlarm'),
+                    onPressed: () {
+                      FlutterRingtonePlayer().playAlarm();
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: ElevatedButton(
+                    child: const Text('playAlarm asAlarm: false'),
+                    onPressed: () {
+                      FlutterRingtonePlayer().playAlarm(asAlarm: false);
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: ElevatedButton(
+                    child: const Text('playNotification'),
+                    onPressed: () {
+                      FlutterRingtonePlayer().playNotification();
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: ElevatedButton(
+                    child: const Text('playRingtone'),
+                    onPressed: () {
+                      FlutterRingtonePlayer().playRingtone();
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: ElevatedButton(
+                    child: const Text('Play from asset (iphone.mp3)'),
+                    onPressed: () {
+                      FlutterRingtonePlayer()
+                          .play(fromAsset: "assets/iphone.mp3");
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: ElevatedButton(
+                    child: const Text('Play from asset (android.wav)'),
+                    onPressed: () {
+                      FlutterRingtonePlayer()
+                          .play(fromAsset: "assets/android.wav");
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: ElevatedButton(
+                    child: const Text('play'),
+                    onPressed: () {
+                      FlutterRingtonePlayer().play(
+                        android: AndroidSounds.notification,
+                        ios: IosSounds.glass,
+                        looping: true,
+                        volume: 1.0,
+                      );
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: ElevatedButton(
+                    child: const Text('stop'),
+                    onPressed: () {
+                      FlutterRingtonePlayer().stop();
+                    },
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
-    );
-  }
-
-  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-      FlutterLocalNotificationsPlugin();
-
-  void requestNotificationPermission() async {
-    if (await Permission.notification.request().isGranted) {
-      showNotification();
-    } else {
-      print("Notification permission denied");
-    }
-  }
-
-  void showNotification() async {
-    var androidDetails = AndroidNotificationDetails(
-      'default_channel_id',
-      'default_channel_name',
-      channelDescription: 'Default notification channel',
-      importance: Importance.max,
-      priority: Priority.high,
-    );
-
-    var platformDetails = NotificationDetails(android: androidDetails);
-
-    await flutterLocalNotificationsPlugin.show(
-      0, 
-      'Test Notification',
-      'This is a notification triggered from the background or foreground.',
-      platformDetails,
     );
   }
 }
